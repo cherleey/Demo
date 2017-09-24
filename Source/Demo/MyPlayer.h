7010bd64e0f18f53e3669220e1092dd61f8145f4
cherleey <31400524+cherleey@users.noreply.github.com>
2017-09-25 03:02:12 +0900

    Initial commit

---
+++ Source/Demo/MyPlayer.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "MyPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMO_API AMyPlayer : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
